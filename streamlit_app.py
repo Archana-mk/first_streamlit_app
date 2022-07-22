8d10dc7beac8f1da630b7caa8f740586a17588b8
Archana M K <109788301+Archana-mk@users.noreply.github.com>
2022-07-22 17:22:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
@@ -6,7 +7,11 @@ streamlit.text('Omega 3 & blueberry oatmeal')
 streamlit.text('kale, spinach & rocket smoothie')
 streamlit.text('hard-boiled free-ranged egg')
 
-streamlit.header('fruit smoothie')
-streamlit.text('apple milkshake')
-streamlit.text('U+1F34C banana milkshake')
-streamlit.text('orange juice')
+streamlit.header('build your own fruit smoothie')
+# streamlit.text('apple milkshake')
+# streamlit.text('U+1F34C banana milkshake')
+# streamlit.text('orange juice')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)

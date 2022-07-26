359b9ee0b086888a8cda9a8e3a8dc249e2c46394
Archana M K <109788301+Archana-mk@users.noreply.github.com>
2022-07-26 17:46:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ streamlit.header('build your own fruit smoothie')
 # streamlit.text('orange juice')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.dataframe(my_fruit_list)
 
